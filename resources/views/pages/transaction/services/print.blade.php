6ef29c7485eb21e4a12459bed0ee845132065d59
lamkhil <43593728+lamkhil@users.noreply.github.com>
2023-09-26 09:52:52 +0700

    UPDATE SURAT EDARAN

--- resources/views/pages/transaction/services/print.blade.php
+++ resources/views/pages/transaction/services/print.blade.php
@@ -118,7 +118,7 @@
                         <div style="width:90%">
                             <h6 style="margin: 0px; font-size:12pt;font-family:Tahoma;">Jl.&nbsp;Merpati&nbsp;No.&nbsp;75,&nbsp;Kota Madiun,&nbsp;63129,&nbsp;Indonesia</h6>
                             <h6 style="margin: 0px; font-size:12pt;font-family:Tahoma;">Telp.:&nbsp;493137</h6>
-                            <h6 style="margin: 0px; font-size:12pt;font-family:Tahoma;">Website:&nbsp;https://madiunkota.go.id</h6>
+                            <h6 style="margin: 0px; font-size:12pt;font-family:Tahoma;">Laman:&nbsp;https://madiunkota.go.id</h6>
                         </div>
                     </center>
                 </td>
@@ -278,7 +278,7 @@
                         @else
                         <b>a.n. LURAH NAMBANGAN LOR</b><br>Sekretaris
                         @endif<br /><br /><br /><br /><br /><br />
-                        <b><u>{{$data->sign_name}}</u></b> <br> {{$data->sign_rank}}<br />NIP. {{$data->sign_id}}
+                        <b><u>{{$data->sign_name}}</u></b> <br> {{$data->sign_rank}}<br />NIP {{$data->sign_id}}
                     </td>
                 </tr>
             </table>
